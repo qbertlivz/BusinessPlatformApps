dddd5bcbdd1e94cfa39ce34d7df2890ad57347bf
Yasin Shtiui <yashti@microsoft.com>
2017-03-23 14:24:28 -0700

    data movement page

--- Source/SiteCommon/Web/pages-gallery/data-movement.ts
+++ Source/SiteCommon/Web/pages-gallery/data-movement.ts
@@ -1,7 +1,17 @@
-﻿import { ViewModelBase } from '../services/view-model-base';
+﻿import { DataStoreType } from '../enums/data-store-type';
 
-export class Customize extends ViewModelBase {
+import { ViewModelBase } from '../services/view-model-base';
+
+export class DataMovement extends ViewModelBase {
+    dataMovement: string = 'ADF';
+
     async OnLoaded(): Promise<void> {
         this.isValidated = true;
+    }
+
+    async NavigatingNext(): Promise<boolean> {
+        this.MS.DataStore.addToDataStore('DataMovement', this.dataMovement, DataStoreType.Public);
+
+        return true;
     }
 }

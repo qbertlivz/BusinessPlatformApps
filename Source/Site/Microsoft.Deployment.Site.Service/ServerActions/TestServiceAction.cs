43a64ac31946cdfc2f30794e6c947edd4bffcdfa
Yasin Shtiui <yashti@microsoft.com>
2017-03-31 11:31:13 -0700

    cleanup Site.Service

--- Source/Site/Microsoft.Deployment.Site.Service/ServerActions/TestServiceAction.cs
+++ Source/Site/Microsoft.Deployment.Site.Service/ServerActions/TestServiceAction.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.Composition;
+﻿using System.ComponentModel.Composition;
 using System.Threading.Tasks;
+
 using Microsoft.Deployment.Common.ActionModel;
 using Microsoft.Deployment.Common.Actions;
-using Newtonsoft.Json.Linq;
 
 namespace Microsoft.Deployment.Site.Service.ServerActions
 {

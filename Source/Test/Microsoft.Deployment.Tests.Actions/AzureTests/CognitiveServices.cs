31b7b5b3c68715de31f9420266a206c47285571e
Mohammad Ali (MOHAALI) <mohaali@microsoft.com>
2017-04-07 10:15:44 -0700

    Fixed cognitive services

--- Source/Test/Microsoft.Deployment.Tests.Actions/AzureTests/CognitiveServices.cs
+++ Source/Test/Microsoft.Deployment.Tests.Actions/AzureTests/CognitiveServices.cs
@@ -20,8 +20,14 @@ namespace Microsoft.Deployment.Tests.Actions.AzureTests
             Assert.IsTrue(subscriptionResult.IsSuccess);
             var subscriptionId = subscriptionResult.Body.GetJObject()["value"].FirstOrDefault(p => p["DisplayName"].ToString().StartsWith("Mohaali"));
             dataStore.AddToDataStore("SelectedSubscription", subscriptionId, DataStoreType.Public);
-
+            dataStore.AddToDataStore("CognitiveServices", "TextAnalytics");
+            dataStore.AddToDataStore("CognitiveLocation", "westus");
             var response = await TestManager.ExecuteActionAsync("Microsoft-RegisterCognitiveServices", dataStore);
+            Assert.IsTrue(response.Status == ActionStatus.Success);
+
+            dataStore.AddToDataStore("CognitiveServices", "Bing.Search");
+            dataStore.AddToDataStore("CognitiveLocation", "global");
+            response = await TestManager.ExecuteActionAsync("Microsoft-RegisterCognitiveServices", dataStore);
             Assert.IsTrue(response.Status == ActionStatus.Success);
         }
     }

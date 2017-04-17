08fa6c698fc657490dff4b91efa43fb305d05b31
Yasin Shtiui <yashti@microsoft.com>
2017-04-17 13:55:04 -0700

    fix datastore for sql

--- Source/Actions/Microsoft.Deployment.Actions.Common/ExistingSqlServer.cs
+++ Source/Actions/Microsoft.Deployment.Actions.Common/ExistingSqlServer.cs
@@ -2,6 +2,7 @@
 using System.ComponentModel.Composition;
 using System.ComponentModel.Composition.Primitives;
 using System.Threading.Tasks;
+
 using Microsoft.Deployment.Common.ActionModel;
 using Microsoft.Deployment.Common.Actions;
 using Microsoft.Deployment.Common.Enums;
@@ -13,15 +14,8 @@ namespace Microsoft.Deployment.Actions.Common
     {
         public override async Task<ActionResponse> ExecuteActionAsync(ActionRequest request)
         {
-            var server = request.DataStore.GetAllJson("SqlCredentials")[0]["Server"].ToString();
-            var database = request.DataStore.GetAllJson("SqlCredentials")[0]["Database"].ToString();
-
-            request.Logger.LogResource(request.DataStore, server,
-                DeployedResourceType.SqlServer, CreatedBy.User, DateTime.UtcNow.ToString("o"));
-
-
-            request.Logger.LogResource(request.DataStore, database,
-                DeployedResourceType.SqlServer, CreatedBy.User, DateTime.UtcNow.ToString("o"));
+            request.Logger.LogResource(request.DataStore, request.DataStore.GetValue("Server"), DeployedResourceType.SqlServer, CreatedBy.User, DateTime.UtcNow.ToString("o"));
+            request.Logger.LogResource(request.DataStore, request.DataStore.GetValue("Database"), DeployedResourceType.SqlServer, CreatedBy.User, DateTime.UtcNow.ToString("o"));
 
             return new ActionResponse(ActionStatus.Success);
         }

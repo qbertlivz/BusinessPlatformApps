ccf86f82d5056c959fc19928e631a0efd2af5dc2
Mohammad Ali (MOHAALI) <mohaali@microsoft.com>
2017-04-18 07:34:48 -0700

    Fixed twitter

---
+++ Source/Test/Microsoft.Deployment.Tests.Actions/AzureTests/ConnectorTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Deployment.Tests.Actions.TestHelpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Deployment.Common.ActionModel;
+
+namespace Microsoft.Deployment.Tests.Actions.AzureTests
+{
+    [TestClass]
+    public class ConnectorTest
+    {
+        [TestMethod]
+        public async Task CreateConnector()
+        {
+            var dataStore = await TestManager.GetDataStore();
+
+            var response = TestManager.ExecuteAction("Microsoft-CreateTwitterConnectionToLogicApp", dataStore);
+            Assert.IsTrue(response.IsSuccess);
+        }
+    }
+}

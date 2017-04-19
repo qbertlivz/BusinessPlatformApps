9d4ac4f7b58f5dbfc93beb7dd01d8372723c2b14
raymondb <raymondb@microsoft.com>
2017-04-19 13:02:00 -0700

    Bring back Scribe data structures

---
+++ Source/Apps/Microsoft/Released/Microsoft-CRMSalesManagement/Database/Scribe/50 post.sql
@@ -0,0 +1,27 @@
+SET ANSI_NULLS              ON;
+SET ANSI_PADDING            ON;
+SET ANSI_WARNINGS           ON;
+SET ANSI_NULL_DFLT_ON       ON;
+SET CONCAT_NULL_YIELDS_NULL ON;
+SET QUOTED_IDENTIFIER       ON;
+go
+
+/************************************
+* Tables to drop                    *
+*************************************/
+
+-- Scribe needs to recreate these tables, however the fields we needed in the views will still be present
+DROP TABLE dbo.account;
+DROP TABLE dbo.businessunit;
+DROP TABLE dbo.lead;
+DROP TABLE dbo.opportunity;
+DROP TABLE dbo.opportunityproduct;
+DROP TABLE dbo.systemuser;
+DROP TABLE dbo.systemusermanagermap;
+DROP TABLE dbo.territory;
+DROP TABLE dbo.scribe_replicationstatus;
+DROP TABLE dbo.product;
+/************************************
+* Tables to truncate                *
+*************************************/
+

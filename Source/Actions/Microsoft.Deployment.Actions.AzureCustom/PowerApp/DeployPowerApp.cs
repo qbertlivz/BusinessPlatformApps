06addb3dabebbda36c5459ee28e84e31bf9bb25c
Yasin Shtiui <yashti@microsoft.com>
2017-04-19 15:37:24 -0700

    provision a blank powerapp

--- Source/Actions/Microsoft.Deployment.Actions.AzureCustom/PowerApp/DeployPowerApp.cs
+++ Source/Actions/Microsoft.Deployment.Actions.AzureCustom/PowerApp/DeployPowerApp.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.Composition;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 
 using Newtonsoft.Json.Linq;
@@ -14,8 +16,11 @@ namespace Microsoft.Deployment.Actions.AzureCustom.PowerApp
     [Export(typeof(IAction))]
     public class DeployPowerApp : BaseAction
     {
-        private string BASE_POWER_APPS_URL = "https://management.azure.com/providers/Microsoft.PowerApps";
-        private string CREATE_POWER_APPS_URL = "https://create.powerapps.com/v2.0.610.0/api";
+        private const string BASE_POWER_APPS_URL = "https://management.azure.com/providers/Microsoft.PowerApps";
+        private const string CREATE_POWER_APPS_URL = "https://create.powerapps.com/v2.0.610.0/api";
+        private const string POWER_APP_GUID_CHARACTERS = "0123456789abcdef";
+
+        private readonly int[] POWER_APP_GUID_LENGTHS = { 8, 4, 4, 4, 12 };
 
         public override async Task<ActionResponse> ExecuteActionAsync(ActionRequest request)
         {
@@ -23,6 +28,7 @@ namespace Microsoft.Deployment.Actions.AzureCustom.PowerApp
 
             AzureHttpClient client = new AzureHttpClient(azureToken);
 
+            string applicationId = GetNewPowerAppGuid();
             string applicationName = "TwitterTemplate" + RandomGenerator.GetDateStamp();
             string environmentId = request.DataStore.GetValue("PowerAppEnvironment");
             string objectId = JsonUtility.GetWebToken(azureToken, "oid");
@@ -42,16 +48,31 @@ namespace Microsoft.Deployment.Actions.AzureCustom.PowerApp
             await clientPA.ExecuteGenericRequestWithHeaderAsync(HttpMethod.Post, $"{CREATE_POWER_APPS_URL}/authoringsession/newinstance?requestedLocation=unitedstates", string.Empty);
             await clientPA.ExecuteGenericRequestWithHeaderAsync(HttpMethod.Get, $"{CREATE_POWER_APPS_URL}/document/util/createdocumentandloadcontext?apptype=0&locale=en-US", string.Empty);
             await clientPA.ExecuteGenericRequestWithHeaderAsync(HttpMethod.Get, $"{CREATE_POWER_APPS_URL}/document/2/getpublishinfo", string.Empty);
-
-            string applicationId = (await clientPA.ExecuteGenericRequestWithHeaderAndReadAsync(HttpMethod.Get, $"{CREATE_POWER_APPS_URL}/document/2/propertiesgetappid?appname=App", string.Empty)).Trim('"');
-
             await clientPA.ExecuteGenericRequestWithHeaderAsync(HttpMethod.Post, $"{CREATE_POWER_APPS_URL}/document/2/setpublishinfo", $"{{\"applicationName\":\"{applicationName}\",\"backgroundColorString\":\"RGBA(0,176,240,1)\",\"logoFile\":{{\"_path\":\"default_icon.png\"}},\"logoFileName\":\"default_icon.png\",\"publishDataLocally\":false,\"publishResourcesLocally\":false,\"publishTarget\":null}}");
             await clientPA.ExecuteGenericRequestWithHeaderAsync(HttpMethod.Post, $"{CREATE_POWER_APPS_URL}/document/2/publishtoblobasync", $"{{\"blobURI\":\"{sharedAccessSignature}\",\"docName\":\"{applicationName}\",\"documentSienaUri\":\"/document.msapp\",\"logoSmallUri\":\"/logoSmallFile\"}}");
-
-            var response = await clientPA.ExecuteGenericRequestWithHeaderAsync(HttpMethod.Post, $"{BASE_POWER_APPS_URL}/apps?api-version=2016-11-01", $"{{\"properties\":{{\"appUris\":{{\"documentUri\":{{\"value\":\"{documentUri}\"}},\"description\":\"\",\"backgroundColor\":\"RGBA(0,176,240,1)\",\"minClientVersion\":{{\"major\":2,\"minor\":0,\"build\":610,\"majorRevision\":0,\"minorRevision\":0,\"revision\":0}},\"createdByClientVersion\":{{\"major\":2,\"minor\":0,\"build\":610,\"majorRevision\":0,\"minorRevision\":0,\"revision\":0}},\"backgroundImageUri\":\"{backgroundImageUri}\",\"displayName\":\"{applicationName}\",\"environment\":{{\"id\":\"/providers/Microsoft.PowerApps/environments/{environmentId}\",\"name\":\"{environmentId}\"}}}},\"tags\":{{\"sienaVersion\":\"687bdfbb-4a77-8384-ecba-80840af4d06a\",\"deviceCapabilities\":\"\",\"supportsPortrait\":\"false\",\"supportsLandscape\":\"true\",\"primaryFormFactor\":\"Tablet\",\"primaryDeviceWidth\":\"1366\",\"primaryDeviceHeight\":\"768\",\"publisherVersion\":\"2.0.610\",\"minimumRequiredApiVersion\":\"2.1.0\"}},\"name\":\"{applicationId}\"}}");
-            var responseString = await response.Content.ReadAsStringAsync();
+            await clientPA.ExecuteGenericRequestWithHeaderAsync(HttpMethod.Post, $"{BASE_POWER_APPS_URL}/apps?api-version=2016-11-01", $"{{\"properties\":{{\"appUris\":{{\"documentUri\":{{\"value\":\"{documentUri}\"}}}},\"description\":\"\",\"backgroundColor\":\"RGBA(0,176,240,1)\",\"minClientVersion\":{{\"major\":2,\"minor\":0,\"build\":610,\"majorRevision\":0,\"minorRevision\":0,\"revision\":0}},\"createdByClientVersion\":{{\"major\":2,\"minor\":0,\"build\":610,\"majorRevision\":0,\"minorRevision\":0,\"revision\":0}},\"backgroundImageUri\":\"{backgroundImageUri}\",\"displayName\":\"{applicationName}\",\"environment\":{{\"id\":\"/providers/Microsoft.PowerApps/environments/{environmentId}\",\"name\":\"{environmentId}\"}}}},\"tags\":{{\"sienaVersion\":\"{GetNewPowerAppGuid()}\",\"deviceCapabilities\":\"\",\"supportsPortrait\":\"false\",\"supportsLandscape\":\"true\",\"primaryFormFactor\":\"Tablet\",\"primaryDeviceWidth\":\"1366\",\"primaryDeviceHeight\":\"768\",\"publisherVersion\":\"2.0.610\",\"minimumRequiredApiVersion\":\"2.1.0\"}},\"name\":\"{applicationId}\"}}");
 
             return new ActionResponse(ActionStatus.Success, JsonUtility.GetEmptyJObject());
+        }
+
+        private string GetNewPowerAppGuid()
+        {
+            Random randy = new Random();
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < POWER_APP_GUID_LENGTHS.Length; i++)
+            {
+                for (int j = 0; j < POWER_APP_GUID_LENGTHS[i]; j++)
+                {
+                    sb.Append(POWER_APP_GUID_CHARACTERS[randy.Next(POWER_APP_GUID_CHARACTERS.Length)]);
+                }
+                if (i < POWER_APP_GUID_LENGTHS.Length - 1)
+                {
+                    sb.Append('-');
+                }
+            }
+
+            return sb.ToString();
         }
     }
 }

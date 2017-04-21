90cf8cc0679c1f4cbb2d99bccfb337890a642389
catdor <catdor@microsoft.com>
2017-04-21 10:59:17 -0700

    test in progress

--- Source/Test/Microsoft.Deployment.Tests.Actions/AzureTests/ConnectorTest.cs
+++ Source/Test/Microsoft.Deployment.Tests.Actions/AzureTests/ConnectorTest.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using Microsoft.Deployment.Tests.Actions.TestHelpers;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Microsoft.Deployment.Common.ActionModel;
+using System.Dynamic;
+using Microsoft.Deployment.Common.Helpers;
 
 namespace Microsoft.Deployment.Tests.Actions.AzureTests
 {
@@ -27,6 +29,19 @@ namespace Microsoft.Deployment.Tests.Actions.AzureTests
             var dataStore = await TestManager.GetDataStore();
 
             var response = TestManager.ExecuteAction("Microsoft-CreateSqlConnector", dataStore);
+
+            dataStore.AddToDataStore("AzureArmFile", "Service/Notifier/notifierLogicApp.json");
+
+            dynamic AzureArmParameters = new ExpandoObject();
+            AzureArmParameters.notifierLogicApp = "notifierLogicApp";
+            AzureArmParameters.sqlConnection = dataStore.GetValue("sqlConnectionName");
+            AzureArmParameters.resourcegroup = dataStore.GetValue("SelectedResourceGroup");
+            AzureArmParameters.subscription = dataStore.GetJson("SelectedSubscription")["SubscriptionId"];
+
+            dataStore.AddToDataStore("AzureArmParameters", JsonUtility.GetJsonStringFromObject(AzureArmParameters));
+
+            response = TestManager.ExecuteAction("Microsoft-DeployAzureArmTemplate", dataStore);
+
             Assert.IsTrue(response.IsSuccess);
         }
 

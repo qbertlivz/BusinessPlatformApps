fbdaa261efe49a6fe102765b49df1e083b6f0609
catdor <catdor@microsoft.com>
2017-04-26 18:07:11 -0700

    crm wip

---
+++ Source/Actions/Microsoft.Deployment.Actions.MsCrm/CrmGetEntityInitialCounts.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Deployment.Common.ActionModel;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Sdk.WebServiceClient;
+
+namespace Microsoft.Deployment.Common.Actions.MsCrm
+{
+    [Export(typeof(IAction))]
+    public class CrmGetEntityInitialCounts : BaseAction
+    {
+        public async override Task<ActionResponse> ExecuteActionAsync(ActionRequest request)
+        {
+            string refreshToken = request.DataStore.GetJson("MsCrmToken")["refresh_token"].ToString();
+            string organizationUrl = request.DataStore.GetValue("OrganizationUrl");
+            string[] entities = request.DataStore.GetValue("Entities").Split(new[] { ',', ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+
+            var crmToken = CrmTokenUtility.RetrieveCrmOnlineToken(refreshToken, request.Info.WebsiteRootUrl, request.DataStore, organizationUrl);
+
+            Dictionary<string, int> initialCounts = new Dictionary<string, int>();
+
+            var proxy = new OrganizationWebProxyClient(new Uri($"{organizationUrl}XRMServices/2011/Organization.svc/web"), true)
+            {
+                HeaderToken = crmToken["access_token"].ToString()
+            };
+
+            foreach(var entry in entities)
+            {
+                var xml = $@"
+                        <fetch distinct='false' mapping='logical' aggregate='true'> 
+                            <entity name='{entry}'> 
+                                <attribute name='{entry}id' alias='{entry}_count' aggregate='count'/> 
+                            </entity> 
+                        </fetch>";
+
+
+                var resultEntity = proxy.RetrieveMultiple(
+                new FetchExpression(xml)).Entities.First();
+                var count = resultEntity
+                  .GetAttributeValue<Microsoft.Xrm.Sdk.AliasedValue>($"{entry}_count").Value;
+
+                initialCounts.Add(entry, Convert.ToInt16(count));
+            }
+
+            return new ActionResponse(ActionStatus.Success);
+        }
+    }
+}
---
+++ Source/Actions/Microsoft.Deployment.Actions.MsCrm/CrmTokenUtility.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Hyak.Common.Internals;
+using Microsoft.Deployment.Common.ActionModel;
+using Microsoft.Deployment.Common.Helpers;
+using Newtonsoft.Json.Linq;
+
+namespace Microsoft.Deployment.Common.Actions.MsCrm
+{
+    public static class CrmTokenUtility
+    {
+        public static JObject RetrieveCrmOnlineToken(string refreshToken, string websiteRootUrl, DataStore dataStore, string resourceUri)
+        {
+            string tokenUrl = string.Format(Constants.AzureTokenUri, dataStore.GetValue("AADTenant"));
+
+            using (HttpClient httpClient = new HttpClient())
+            {
+                // CRM Online token
+                string token = GetDynamicsResourceUri(refreshToken, resourceUri, websiteRootUrl, Constants.MsCrmClientId);
+                StringContent content = new StringContent(token);
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/x-www-form-urlencoded");
+                string response = httpClient.PostAsync(new Uri(tokenUrl), content).Result.Content.AsString();
+
+                return JsonUtility.GetJsonObjectFromJsonString(response);
+            }
+        }
+
+        private static string GetDynamicsResourceUri(string code, string uri, string rootUrl, string clientId)
+        {
+            return $"refresh_token={code}&" +
+                   $"client_id={clientId}&" +
+                   $"client_secret={Uri.EscapeDataString(Constants.MicrosoftClientSecret)}&" +
+                   $"resource={Uri.EscapeDataString(uri)}&" +
+                   $"redirect_uri={Uri.EscapeDataString(rootUrl + Constants.WebsiteRedirectPath)}&" +
+                   "grant_type=refresh_token";
+        }
+    }
+}

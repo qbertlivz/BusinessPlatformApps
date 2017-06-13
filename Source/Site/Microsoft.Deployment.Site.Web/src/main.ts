812891c8eb40b0225c35c460b8c28bc099421dba
Yasin Shtiui <yashti@microsoft.com>
2017-06-13 09:59:16 -0700

    skip notifier bit if not adf instead of not informatica

--- Source/Site/Microsoft.Deployment.Site.Web/src/main.ts
+++ Source/Site/Microsoft.Deployment.Site.Web/src/main.ts
@@ -3,7 +3,7 @@
 import { Aurelia } from 'aurelia-framework';
 
 export function configure(aurelia: Aurelia) {
-    aurelia.use.standardConfiguration();
-    //aurelia.use.standardConfiguration().developmentLogging();
+    //aurelia.use.standardConfiguration();
+    aurelia.use.standardConfiguration().developmentLogging();
     aurelia.start().then(() => aurelia.setRoot());
 }

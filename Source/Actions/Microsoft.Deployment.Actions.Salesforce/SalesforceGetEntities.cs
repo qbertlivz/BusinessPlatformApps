08ba210c98c7da88b1f0bbe53dd83ec53d5b83ed
catdor <catdor@microsoft.com>
2017-06-20 10:37:16 -0700

    sfdc additional entities and tests

---
+++ Source/Actions/Microsoft.Deployment.Actions.Salesforce/SalesforceGetEntities.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.Deployment.Common.Actions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Deployment.Common.ActionModel;
+using Microsoft.Deployment.Common.Helpers;
+using Microsoft.Deployment.Actions.Salesforce.SalesforceSOAP;
+using Microsoft.Deployment.Actions.Salesforce.Helpers;
+using System.Dynamic;
+using System.ServiceModel;
+using System.Diagnostics;
+
+namespace Microsoft.Deployment.Actions.Salesforce
+{
+    [Export(typeof(IAction))]
+    public class SalesforceGetEntities : BaseAction
+    {
+        public override async Task<ActionResponse> ExecuteActionAsync(ActionRequest request)
+        {
+            string sfUsername = request.DataStore.GetValue("SalesforceUser");
+            string sfPassword = request.DataStore.GetValue("SalesforcePassword");
+            string sfToken = request.DataStore.GetValue("SalesforceToken");
+            string sfTestUrl = request.DataStore.GetValue("SalesforceUrl");
+            List<string> sfObjects = new List<string>();
+
+            SoapClient binding = new SoapClient("Soap");
+
+            if (!string.IsNullOrEmpty(sfTestUrl) && sfTestUrl.Contains("test"))
+            {
+                binding.Endpoint.Address = new System.ServiceModel.EndpointAddress(binding.Endpoint.Address.ToString().Replace("login", "test"));
+            }
+
+            LoginResult lr;
+
+            SecurityHelper.SetTls12();
+
+            binding.ClientCredentials.UserName.UserName = sfUsername;
+            binding.ClientCredentials.UserName.Password = sfPassword;
+
+            lr =
+               binding.login(null, null,
+               sfUsername,
+               string.Concat(sfPassword, sfToken));
+
+            dynamic metadata = new ExpandoObject();
+
+            binding = new SoapClient("Soap");
+            metadata.Objects = new List<DescribeSObjectResult>();
+            SessionHeader sheader = new SessionHeader();
+            BasicHttpBinding bind = new BasicHttpBinding();
+            bind = (BasicHttpBinding)binding.Endpoint.Binding;
+            bind.MaxReceivedMessageSize = int.MaxValue;
+            bind.MaxBufferPoolSize = int.MaxValue;
+            bind.MaxBufferSize = int.MaxValue;
+            bind.CloseTimeout = new TimeSpan(0, 0, 5, 0);
+            bind.OpenTimeout = new TimeSpan(0, 0, 5, 0);
+            bind.SendTimeout = new TimeSpan(0, 0, 5, 0);
+            bind.ReaderQuotas.MaxArrayLength = int.MaxValue;
+            bind.ReaderQuotas.MaxDepth = int.MaxValue;
+            bind.ReaderQuotas.MaxNameTableCharCount = int.MaxValue;
+            bind.ReaderQuotas.MaxStringContentLength = int.MaxValue;
+            bind.ReaderQuotas.MaxBytesPerRead = int.MaxValue;
+            bind.ReaderQuotas.MaxNameTableCharCount = int.MaxValue;
+
+            binding.Endpoint.Binding = bind;
+            binding.Endpoint.Address = new EndpointAddress(lr.serverUrl);
+
+            sheader.sessionId = lr.sessionId;
+
+            binding.Endpoint.ListenUri = new Uri(lr.metadataServerUrl);
+
+            DescribeGlobalResult result;
+            binding.describeGlobal(sheader, null, null, out result);
+
+            foreach (var obj in result.sobjects)
+            {
+                sfObjects.Add(obj.name);
+            }
+
+            request.DataStore.AddObjectDataStore("sfdcOjects", JsonUtility.GetJObjectFromObject(sfObjects), DataStoreType.Public);
+
+            return new ActionResponse(ActionStatus.Success, JsonUtility.GetJObjectFromObject(metadata));
+        }
+    }
+}
---
+++ Source/Test/Microsoft.Deployment.Tests.Actions/SalesforceTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Deployment.Common.ActionModel;
+using Microsoft.Deployment.Tests.Actions.TestHelpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Deployment.Tests.Actions
+{
+    [TestClass]
+    public class SalesforceUnitTests
+    {
+        [TestMethod]
+        public void GetAllObjects()
+        {
+            var ds = new DataStore();
+            ds.AddToDataStore("ObjectTables", "");
+            ds.AddToDataStore("SalesforceUser", Credential.Instance.Salesforce.Username);
+            ds.AddToDataStore("SalesforcePassword", Credential.Instance.Salesforce.Password);
+            ds.AddToDataStore("SalesforceToken", Credential.Instance.Salesforce.Token);
+            ds.AddToDataStore("SalesforceUrl", "https://login.salesforce.com");
+
+            var resp = TestManager.ExecuteAction("Microsoft-SalesforceGetEntities", ds);
+        }
+    }
+}

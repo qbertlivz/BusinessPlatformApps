b3b79d9786288761b2ea53dfe5f1208eb50b5809
Mohammad Ali (MOHAALI) <mohaali@microsoft.com>
2017-06-23 09:45:48 -0700

    Fixes to FB template

--- Source/Apps/Microsoft/Released/Microsoft-FacebookTemplate/Database/20 tables.sql
+++ Source/Apps/Microsoft/Released/Microsoft-FacebookTemplate/Database/20 tables.sql
@@ -6,71 +6,53 @@ SET CONCAT_NULL_YIELDS_NULL ON;
 SET QUOTED_IDENTIFIER       ON;
 
 CREATE TABLE [fb].[HashTags](
-	[Id1] [bigint] NULL,
-	[Id2] [bigint] NULL,
-	[Original Id] [nvarchar](50) NULL,
+	[Id] [nvarchar](50) NULL,
 	[HashTags] [nvarchar](100) NULL
 )
 
 CREATE TABLE [fb].[KeyPhrase](
-	[Id1] [bigint] NULL,
-	[Id2] [bigint] NULL,
-	[Original Id] [nvarchar](50) NULL,
+	[Id] [nvarchar](50) NULL,
 	[KeyPhrase] [nvarchar](100) NULL
 )
 
 CREATE TABLE [fb].[Sentiment](
-	[Id1] [bigint] NULL,
-	[Id2] [bigint] NULL,
-	[Original Id] [nvarchar](50) NULL,
+	[Id] [nvarchar](50) NULL,
 	[Sentiment] [float] NULL
 )
 
 
 CREATE TABLE [fb].[Reactions](
-	[Id1] [bigint] NULL,
-	[Id2] [bigint] NULL,
-	[Original Id] [nvarchar](50) NULL,
+	[Id] [nvarchar](50) NULL,
 	[Reaction Type] [nvarchar](30) NULL,
-	[From Id] [bigint] NULL,
-	[From Name] [nvarchar](100) NULL
+	[Count] [bigint] NULL,
 )
 
 CREATE TABLE [fb].[Comments](
-	[Id1] [bigint] NULL,
-	[Id2] [bigint] NULL,
-	[Original Id] [nvarchar](50) NULL,
+	[Id] [nvarchar](50) NULL,
 	[Created Date] [datetime] NULL,
 	[Message] [nvarchar](max) NULL,
-	[From Id] [bigint] NULL,
+	[From Id] [nvarchar](50) NULL,
 	[From Name] [nvarchar](100) NULL,
-	[Post Id1] [bigint] NULL,
-	[Post Id2] [bigint] NULL,
-	[Original Post Id] [nvarchar](50) NULL,
-	[Page] [nchar](100) NULL,
-    [PageDisplayName] [nchar](200) NULL,
-    [PageId] [bigint] NULL
+	[Post Id] [nvarchar](50) NULL,
+	[Page] [nvarchar](100) NULL,
+    [PageDisplayName] [nvarchar](200) NULL,
+    [PageId] [nvarchar](50) NULL
 )
 
 CREATE TABLE [fb].[Posts](
-	[Id1] [bigint] NULL,
-	[Id2] [bigint] NULL,
-	[Original Id] [nvarchar](50) NULL,
+	[Id] [nvarchar](50) NULL,
 	[Created Date] [datetime] NULL,
 	[Message] [nvarchar](max) NULL,
-	[From Id] [bigint] NULL,
+	[From Id] [nvarchar](50) NULL,
 	[From Name] [nvarchar](200) NULL,
 	[Media] [nvarchar](1000) NULL,
-	[Total Likes] [int] NULL,
-	[Total Shares] [int] NULL,
-	[Total Reactions] [int] NULL,
-	[Page] [nvarchar](100) NULL,
-    [PageDisplayName] [nchar](200) NULL,
-    [PageId] [bigint] NULL,
+    [Page] [nvarchar](100) NULL,
+    [PageDisplayName] [nvarchar](200) NULL,
+    [PageId] [nvarchar](50) NULL,
 	[Total Comments] [int] NULL
 )
 
-CREATE TABLE [fb].[configuration](
+CREATE TABLE [fb].[Configuration](
 	[id] [int] IDENTITY(1,1) NOT NULL,
 	[configuration_group] [varchar](150) NOT NULL,
 	[configuration_subgroup] [varchar](150) NOT NULL,
@@ -79,7 +61,7 @@ CREATE TABLE [fb].[configuration](
 	[visible] [bit] NOT NULL
 )
 
-CREATE TABLE [fb].[date](
+CREATE TABLE [fb].[Date](
 	[date_key] [int] NOT NULL,
 	[full_date] [date] NOT NULL,
 	[day_of_week] [tinyint] NOT NULL,
@@ -103,20 +85,15 @@ CREATE TABLE [fb].[date](
 )
 
 CREATE TABLE [fb].[StagingComments](
-	[Id1] [bigint] NULL,
-	[Id2] [bigint] NULL,
-	[Original Id] [nvarchar](50) NULL,
+	[Id] [nvarchar](50) NULL,
 	[Created Date] [datetime] NULL,
 	[Message] [nvarchar](max) NULL,
-	[From Id] [bigint] NULL,
+	[From Id] [nvarchar](50) NULL,
 	[From Name] [nvarchar](100) NULL,
-	[BulkInsertId] [int] NULL,
-	[Post Id1] [bigint] NULL,
-	[Post Id2] [bigint] NULL,
-	[Original Post Id] [nvarchar](50) NULL,
-	[Page] [nchar](100) NULL,
-    [PageDisplayName] [nchar](200) NULL,
-    [PageId] [bigint] NULL
+	[Post Id] [nvarchar](50) NULL,
+    [Page] [nvarchar](100) NULL,
+    [PageDisplayName] [nvarchar](200) NULL,
+    [PageId] [nvarchar](50) NULL
 )
 
 CREATE TABLE [fb].[StagingError](
@@ -126,60 +103,37 @@ CREATE TABLE [fb].[StagingError](
 )
 
 CREATE TABLE [fb].[StagingHashTags](
-	[Id1] [bigint] NULL,
-	[Id2] [bigint] NULL,
-	[Original Id] [nvarchar](50) NULL,
-	[BulkInsertId] [int] NULL,
+	[Id] [nvarchar](50) NULL,
 	[HashTags] [nvarchar](100) NULL
 )
 
-CREATE TABLE [fb].[StagingInsert](
-	[Id] [int] IDENTITY(1,1) NOT NULL
-)
-
 CREATE TABLE [fb].[StagingKeyPhrase](
-	[Id1] [bigint] NULL,
-	[Id2] [bigint] NULL,
-	[Original Id] [nvarchar](50) NULL,
-	[BulkInsertId] [int] NULL,
+	[Id] [nvarchar](50) NULL,
 	[KeyPhrase] [nvarchar](100) NULL
 )
 
 CREATE TABLE [fb].[StagingPosts](
-	[Id1] [bigint] NULL,
-	[Id2] [bigint] NULL,
-	[Original Id] [nvarchar](50) NULL,
+	[Id] [nvarchar](50) NULL,
 	[Created Date] [datetime] NULL,
 	[Message] [nvarchar](max) NULL,
-	[From Id] [bigint] NULL,
+	[From Id] [nvarchar](50) NULL,
 	[From Name] [nvarchar](200) NULL,
 	[Media] [nvarchar](1000) NULL,
-	[Total Likes] [int] NULL,
-	[Total Shares] [int] NULL,
-	[Total Reactions] [int] NULL,
 	[Page] [nvarchar](100) NULL,
-    [PageDisplayName] [nchar](200) NULL,
-    [PageId] [bigint] NULL,
+    [PageDisplayName] [nvarchar](200) NULL,
+    [PageId] [nvarchar](50) NULL,
 	[Total Comments] [int] NULL,
-	[BulkInsertId] [int] NULL
 )
 
 CREATE TABLE [fb].[StagingReactions](
-	[Id1] [bigint] NULL,
-	[Id2] [bigint] NULL,
-	[Original Id] [nvarchar](50) NULL,
+	[Id] [nvarchar](50) NULL,
 	[Reaction Type] [nvarchar](30) NULL,
-	[From Id] [bigint] NULL,
-	[From Name] [nvarchar](100) NULL,
-	[BulkInsertId] [int] NULL
+	[Count] [bigint] NULL,
 )
 
 
 CREATE TABLE [fb].[StagingSentiment](
-	[Id1] [bigint] NULL,
-	[Id2] [bigint] NULL,
-	[Original Id] [nvarchar](50) NULL,
-	[BulkInsertId] [int] NULL,
+	[Id] [nvarchar](50) NULL,
 	[Sentiment] [float] NULL
 )
 
@@ -194,7 +148,7 @@ CREATE TABLE [fb].[Edges](
 	[SourceVertex] [bigint] NULL,
     [TargetVertex] [bigint] NULL,
     [EdgeWeight] [int] NULL,
-	[PageId] [bigint] NULL
+	[PageId] [nvarchar](50) NULL,
 )
 GO
 
@@ -210,30 +164,26 @@ CREATE NONCLUSTERED INDEX [UserId] ON [fb].[Users]
 
 CREATE NONCLUSTERED INDEX [StagingCommentsId1] ON [fb].[Comments]
 (
-	[Id1] ASC
+	[Id] ASC
 )
-INCLUDE ( 	[Id2]) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)
 GO
 
 CREATE NONCLUSTERED INDEX [StagingCommentsPid1] ON [fb].[Comments]
 (
-	[Post Id1] ASC
+	[Post Id] ASC
 )
-INCLUDE ( 	[Post Id2]) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)
 GO
 
 CREATE NONCLUSTERED INDEX [IX_HashTags] ON [fb].[HashTags]
 (
-	[Id1] ASC
+	[Id] ASC
 )
-INCLUDE ( 	[Id2]) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)
 GO
 
 CREATE NONCLUSTERED INDEX [IX_KeyPhrase] ON [fb].[KeyPhrase]
 (
-	[Id1] ASC
+	[Id] ASC
 )
-INCLUDE ( 	[Id2]) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)
 GO
 
 
@@ -246,87 +196,50 @@ GO
 
 CREATE NONCLUSTERED INDEX [PostsId1] ON [fb].[Posts]
 (
-	[Id1] ASC
+	[Id] ASC
 )
-INCLUDE ( 	[Id2]) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)
 GO
 
 
 CREATE NONCLUSTERED INDEX [IX_Reactions] ON [fb].[Reactions]
 (
-	[Id1] ASC
+	[Id] ASC
 )
-INCLUDE ( 	[Id2]) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)
 GO
 
 
 CREATE NONCLUSTERED INDEX [IX_Sentiment] ON [fb].[Sentiment]
 (
-	[Id1] ASC
+	[Id] ASC
 )
-INCLUDE ( 	[Id2]) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)
 GO
-
-
-CREATE NONCLUSTERED INDEX [StagingCommentsBulkInsert] ON [fb].[StagingComments]
-(
-	[BulkInsertId] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)
-GO
-
 
 CREATE NONCLUSTERED INDEX [StagingCommentsId1] ON [fb].[StagingComments]
 (
-	[Id1] ASC
+	[Id] ASC
 )
-INCLUDE ( 	[Id2]) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)
 GO
 
 
 CREATE NONCLUSTERED INDEX [StagingCommentsPid1] ON [fb].[StagingComments]
 (
-	[Post Id1] ASC
+	[Post Id] ASC
 )
-INCLUDE ( 	[Post Id2]) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)
 GO
 
 
 CREATE NONCLUSTERED INDEX [IX_StagingHashTags] ON [fb].[StagingHashTags]
 (
-	[Id1] ASC
-)
-INCLUDE ( 	[Id2]) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)
-GO
-
-
-CREATE NONCLUSTERED INDEX [IX_StagingHashTags_2] ON [fb].[StagingHashTags]
-(
-	[BulkInsertId] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)
-GO
-
-
-CREATE NONCLUSTERED INDEX [StagingInsertId] ON [fb].[StagingInsert]
-(
 	[Id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)
+)
 GO
 
 
 CREATE NONCLUSTERED INDEX [IX_StagingKeyPhrase] ON [fb].[StagingKeyPhrase]
 (
-	[Id1] ASC
+	[Id] ASC
 )
-INCLUDE ( 	[Id2]) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)
 GO
-
-
-CREATE NONCLUSTERED INDEX [StagingPostsBulkInsert] ON [fb].[StagingPosts]
-(
-	[BulkInsertId] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)
-GO
-
 
 CREATE NONCLUSTERED INDEX [StagingPostsDate] ON [fb].[StagingPosts]
 (
@@ -337,39 +250,24 @@ GO
 
 CREATE NONCLUSTERED INDEX [StagingPostsId1] ON [fb].[StagingPosts]
 (
-	[Id1] ASC
+	[Id] ASC
 )
-INCLUDE ( 	[Id2]) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)
 GO
 
 
 CREATE NONCLUSTERED INDEX [IX_StagingReactions] ON [fb].[StagingReactions]
 (
-	[Id1] ASC
+	[Id] ASC
 )
-INCLUDE ( 	[Id2]) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)
-GO
-
-
-CREATE NONCLUSTERED INDEX [IX_StagingReactions_2] ON [fb].[StagingReactions]
-(
-	[BulkInsertId] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)
 GO
 
 
 CREATE NONCLUSTERED INDEX [IX_StagingSentiment] ON [fb].[StagingSentiment]
 (
-	[Id1] ASC
+	[Id] ASC
 )
-INCLUDE ( 	[Id2]) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)
 GO
 
-CREATE NONCLUSTERED INDEX [IX_StagingSentiment_2] ON [fb].[StagingSentiment]
-(
-	[BulkInsertId] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)
-GO
 
 CREATE NONCLUSTERED INDEX [UserId1] ON [fb].[StagingComments]
 (

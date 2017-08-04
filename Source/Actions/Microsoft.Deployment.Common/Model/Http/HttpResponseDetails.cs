8c01091276c46e72d84fb68d85899c143208678b
Yasin Shtiui <yashti@microsoft.com>
2017-08-04 11:05:10 -0700

    fix validate twitter handle and validate cognitive key

---
+++ Source/Actions/Microsoft.Deployment.Common/Model/Http/HttpResponseDetails.cs
@@ -0,0 +1,16 @@
+﻿using System.Net;
+
+namespace Microsoft.Deployment.Common.Model.Http
+{
+    public class HttpResponseDetails
+    {
+        public HttpStatusCode Code;
+        public string Json;
+
+        public HttpResponseDetails(HttpStatusCode code, string json)
+        {
+            Code = code;
+            Json = json;
+        }
+    }
+}
---
+++ Source/Test/Microsoft.Deployment.Tests.Actions/TwitterTests/TwitterTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+
+using Microsoft.Deployment.Common.ActionModel;
+using Microsoft.Deployment.Tests.Actions.TestHelpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.Deployment.Tests.Actions.Twitter
+{
+    [TestClass]
+    public class TwitterTests
+    {
+        [TestMethod]
+        public async Task ValidateCognitiveKey()
+        {
+            var ds = new DataStore();
+
+            ds.AddToDataStore("CognitiveServiceKey", "d06016d1b742488eb7ff03edeb5020aa");
+
+            Assert.IsTrue(await TestManager.IsSuccessAsync("Microsoft-ValidateCognitiveKey", ds));
+        }
+
+        [TestMethod]
+        public async Task ValidateTwitterAccount()
+        {
+            var ds = new DataStore();
+
+            ds.AddToDataStore("Accounts", "@mohaali45");
+
+            Assert.IsTrue(await TestManager.IsSuccessAsync("Microsoft-ValidateTwitterAccount", ds));
+        }
+    }
+}

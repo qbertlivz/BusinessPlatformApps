88efbcab388eb371b69602f39a2ddfd3f8f2221c
Mohammad Ali (MOHAALI) <mohaali@microsoft.com>
2017-08-07 06:46:05 -0700

    Added the FB Utillities

---
+++ Function/FacebookUtillity/DateUtility.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace FacebookETL
+{
+    public class DateUtility
+    {
+        public static string GetDate(string daysAsString)
+        {
+            // Set name to query string or body data
+            int days = int.Parse(daysAsString);
+
+            if (days > 0)
+            {
+                days = days * -1;
+            }
+
+            string dateToReturn = DateTime.Now.AddDays(days).ToString();
+            return dateToReturn;
+        }
+
+        public static string GetUnixFromDate(string date)
+        {
+            // Get request body
+            DateTime dateTime = DateTime.Parse(date);
+            DateTime epoch = new DateTime(1970, 1, 1, 0, 0, 0, 0);
+            TimeSpan span = (dateTime.ToUniversalTime() - epoch);
+            string timeStamp = Math.Floor(span.TotalSeconds).ToString();
+            return timeStamp;
+        }
+
+        public static string GetDateTimeRelativeFromNow(int days)
+        {
+            if (days > 0)
+            {
+                days = days * -1;
+            }
+
+            return DateTime.Now.AddDays(days).ToString();
+        }
+
+        public static string GetDateTimeRelativeFromNow(string date, int days)
+        {
+            if (days > 0)
+            {
+                days = days * -1;
+            }
+
+            return DateTime.Parse(date).AddDays(days).ToString();
+        }
+    }
+}

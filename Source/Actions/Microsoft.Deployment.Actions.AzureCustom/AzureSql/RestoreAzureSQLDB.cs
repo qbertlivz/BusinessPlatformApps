2d7692ea512031651ab966d332bca808504af1ea
raymondb <raymondb@microsoft.com>
2017-08-08 17:46:40 -0700

    Edfi in progress

--- Source/Actions/Microsoft.Deployment.Actions.AzureCustom/AzureSql/RestoreAzureSQLDB.cs
+++ Source/Actions/Microsoft.Deployment.Actions.AzureCustom/AzureSql/RestoreAzureSQLDB.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Deployment.Common.ActionModel;
 using Microsoft.Deployment.Common.Actions;
+using Microsoft.WindowsAzure.Storage;
 using System.ComponentModel.Composition;
 using System.Threading.Tasks;
 
@@ -15,8 +16,38 @@ namespace Microsoft.Deployment.Actions.AzureCustom.AzureSql
             string targetConnectionString = request.DataStore.GetAllValues("SqlConnectionString")[0];
 
             DAC.DacServices dacService = new DAC.DacServices(targetConnectionString);
-            // DAC.BacPackage backup = DAC.BacPackage.Load(); Stream here
-            
+            CloudStorageAccount csAcc = null;
+
+            /*
+
+
+
+
+                                const string odsAssetsStorageBaseUrl = "https://odsassets.blob.core.windows.net/public/release";
+                    const string odsLatestVersionUrl = odsAssetsStorageBaseUrl + "/LatestVersion.txt";
+                    const string targetConnectionString = "Data Source=pbisttest.database.windows.net;Initial Catalog=raymondb;User ID=pbiadmin;Password=Arius.325";
+
+
+                    static void Main(string[] args)
+                    {
+                        CloudBlobClient blobClient = new CloudBlobClient(new Uri(odsAssetsStorageBaseUrl));
+                        CloudBlob blob = (CloudBlob)blobClient.GetBlobReferenceFromServer(new Uri(odsLatestVersionUrl));
+
+                        string latestVersion = string.Empty;
+                        using (StreamReader sr = new StreamReader(blob.OpenRead()))
+                        {
+                            latestVersion = sr.ReadLine();
+                        }
+
+                        string sqlbacPack = $"{odsAssetsStorageBaseUrl}/{latestVersion}/EdFi_Ods_Populated_Template.bacpac";
+                        blob = (CloudBlob)blobClient.GetBlobReferenceFromServer(new Uri(sqlbacPack));
+                        DAC.DacServices dacService = new DAC.DacServices(targetConnectionString);
+                        using (DAC.BacPackage bp = DAC.BacPackage.Load(blob.OpenRead()))
+                        {
+                            dacService.ImportBacpac(bp, "raymondb");
+                        }
+             */
+
 
             return new ActionResponse(ActionStatus.Success);
         }

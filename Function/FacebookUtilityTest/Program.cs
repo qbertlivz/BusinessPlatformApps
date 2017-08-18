5640fff5f66a7aeb6e1bf36a681ae36d86480503
Cat Doroftei <catdor@microsoft.com>
2017-08-18 14:32:02 +0300

    facebook page etl

--- Function/FacebookUtilityTest/Program.cs
+++ Function/FacebookUtilityTest/Program.cs
@@ -21,16 +21,33 @@ namespace FacebookUtilityTest
             string page = "";
             string accessToken = "";
             string until = DateTime.UtcNow.AddDays(-2).ToString();
-            
-            var clicks = FacebookUtility.GetPageMetricAnalytics(page, until, accessToken,FacebookPageAnalyticsMetricGroups.PageCtaClicks).Result;
+
+            var clicks = FacebookUtility.GetPageMetricAnalytics(page, until, accessToken, FacebookPageAnalyticsMetricGroups.PageCtaClicks).Result;
+            PageAnalyticsETL.PopulateSingleValues(DataTableUtility.GetClicksDataTable(), clicks);
+
             var engagement = FacebookUtility.GetPageMetricAnalytics(page, until, accessToken, FacebookPageAnalyticsMetricGroups.PageEngagement).Result;
+            PageAnalyticsETL.PopulateNestedValues(DataTableUtility.GetEngagementTable(), engagement);
+
             var impressions = FacebookUtility.GetPageMetricAnalytics(page, until, accessToken, FacebookPageAnalyticsMetricGroups.PageImpressions).Result;
-            var pagePostReactions = FacebookUtility.GetPageMetricAnalytics(page, until, accessToken, FacebookPageAnalyticsMetricGroups.PagePostReactions).Result;
+            PageAnalyticsETL.PopulateNestedValues(DataTableUtility.GetImpressionsTable(), impressions);
+
+            //var pagePostReactions = FacebookUtility.GetPageMetricAnalytics(page, until, accessToken, FacebookPageAnalyticsMetricGroups.PagePostReactions).Result;
+            //PageAnalyticsETL.PopulateNestedValues(DataTableUtility.GetPagePost(), impressions);
+
             var pagePosts = FacebookUtility.GetPageMetricAnalytics(page, until, accessToken, FacebookPageAnalyticsMetricGroups.PagePosts).Result;
+            PageAnalyticsETL.PopulateSingleValues(DataTableUtility.GetPagePostsTable(), pagePosts);
+
             var pagePostStories = FacebookUtility.GetPageMetricAnalytics(page, until, accessToken, FacebookPageAnalyticsMetricGroups.PagePostStoriesAndPeopleTalkingAboutThis).Result;
+            PageAnalyticsETL.PopulateSingleValues(DataTableUtility.GetPagePostStoriesTable(), pagePostStories);
+
             var pageReactions = FacebookUtility.GetPageMetricAnalytics(page, until, accessToken, FacebookPageAnalyticsMetricGroups.PageReactions).Result;
+            PageAnalyticsETL.PopulateNestedValues(DataTableUtility.GetPageReactionsTable(), pageReactions);
+
             var pageUserDemographics = FacebookUtility.GetPageMetricAnalytics(page, until, accessToken, FacebookPageAnalyticsMetricGroups.PageUserDemographics).Result;
+            PageAnalyticsETL.PopulateSingleValues(DataTableUtility.GetPageUserDemographicsTable(), pageUserDemographics);
+
             var pageViews = FacebookUtility.GetPageMetricAnalytics(page, until, accessToken, FacebookPageAnalyticsMetricGroups.PageViews).Result;
+            PageAnalyticsETL.PopulateNestedValues(DataTableUtility.GetPageViewsTable(), pageViews);
         }
     }
 }
---
+++ Function/FacebookUtillity/PageAnalyticsETL.cs
@@ -0,0 +1,79 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacebookUtillity
+{
+    public class PageAnalyticsETL
+    {
+        public static void PopulateSingleValues(DataTable table, List<JObject> objects)
+        {
+            foreach (var postPayload in objects)
+            {
+                foreach (var post in postPayload["data"])
+                {
+                    foreach (var val in post["values"])
+                    {
+                        DataRow postRow = table.NewRow();
+                        postRow["EndTime"] = val["end_time"];
+                        postRow["Name"] = post["name"];
+                        postRow["Value"] = val["value"];
+                        postRow["Period"] = post["period"];
+                        postRow["Title"] = post["title"];
+                        postRow["Id"] = post["id"];
+
+                        table.Rows.Add(postRow);
+                    }
+                }
+            }
+        }
+
+        public static void PopulateNestedValues(DataTable table, List<JObject> objects)
+        {
+            foreach (var postPayload in objects)
+            {
+                foreach (var post in postPayload["data"])
+                {
+                    foreach (var val in post["values"])
+                    {
+                        if (val?["value"] != null &&val["value"].Children().Count() > 1)
+                        {
+                            foreach (var child in val["value"])
+                            {
+                                var att = child as JProperty;
+
+                                DataRow postRow = table.NewRow();
+                                postRow["EndTime"] = val["end_time"];
+                                postRow["Name"] = post["name"];
+                                postRow["Entry Name"] = att.Name;
+                                postRow["Value"] = att.Value;
+                                postRow["Period"] = post["period"];
+                                postRow["Title"] = post["title"];
+                                postRow["Id"] = post["id"];
+
+                                table.Rows.Add(postRow);
+                            }
+                        }
+                        else
+                        {
+                            DataRow postRow = table.NewRow();
+                            postRow["EndTime"] = val["end_time"];
+                            postRow["Name"] = post["name"];
+                            postRow["Value"] = val["value"];
+                            postRow["Period"] = post["period"];
+                            postRow["Title"] = post["title"];
+                            postRow["Id"] = post["id"];
+
+                            table.Rows.Add(postRow);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}

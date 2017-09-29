a97fbd832d3d60baae2451389c8c202c4e2790b9
Yasin Shtiui <yashti@microsoft.com>
2017-09-28 17:33:43 -0700

    fix set consent for download buttons

--- Source/SiteCommon/Web/services/view-model-base.ts
+++ Source/SiteCommon/Web/services/view-model-base.ts
@@ -8,6 +8,7 @@ import { InitParser } from './init-parser';
 import { MainService } from './main-service';
 
 export class ViewModelBase {
+    downloadLink: string = null;
     hasConsent: boolean = false;
     isActivated: boolean = false;
     isAuthenticated: boolean = true;
@@ -59,6 +60,11 @@ export class ViewModelBase {
 
     determineActivationStrategy(): any {
         return activationStrategy.replace;
+    }
+
+    downloadMsi(): void {
+        this.setConsent();
+        window.open(this.viewmodel.downloadLink, '_blank');
     }
 
     loadParameters(): void {

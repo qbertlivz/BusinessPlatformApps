5cc4c82a8496937c94d6bfe50b65e268f0d9f8c6
Yasin Shtiui <yashti@microsoft.com>
2017-10-26 15:18:28 -0700

    changes

--- Source/Actions/Microsoft.Deployment.Actions.AzureCustom/ActivityLogs/ExportActivityLogToEventHub.cs
+++ Source/Actions/Microsoft.Deployment.Actions.AzureCustom/ActivityLogs/ExportActivityLogToEventHub.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.Deployment.Common.ActionModel;
 using Microsoft.Deployment.Common.Actions;
 using Microsoft.Deployment.Common.Helpers;
+using Microsoft.Deployment.Common.Model.ActivityLog;
 using Microsoft.Deployment.Common.Model.Bpst;
 
 namespace Microsoft.Deployment.Actions.AzureCustom.Common
@@ -18,15 +19,15 @@ namespace Microsoft.Deployment.Actions.AzureCustom.Common
         {
             BpstAzure ba = new BpstAzure(request.DataStore);
 
-            string nameLogProfile = RandomGenerator.GetRandomHexadecimal("bpst-al-lp-");
             string nameNamespace = request.DataStore.GetValue("nameNamespace");
 
-            string body = $"{{\"id\":null,\"location\":null,\"name\":null,\"properties\":{{\"categories\":[\"Write\",\"Delete\",\"Action\"],\"storageAccountId\":null,\"locations\":[\"australiaeast\",\"australiasoutheast\",\"brazilsouth\",\"canadacentral\",\"canadaeast\",\"centralindia\",\"centralus\",\"eastasia\",\"eastus\",\"eastus2\",\"japaneast\",\"japanwest\",\"koreacentral\",\"koreasouth\",\"northcentralus\",\"northeurope\",\"southcentralus\",\"southindia\",\"southeastasia\",\"uksouth\",\"ukwest\",\"westcentralus\",\"westeurope\",\"westindia\",\"westus\",\"westus2\",\"global\"],\"retentionPolicy\":{{\"enabled\":false,\"days\":0}},\"serviceBusRuleId\":\"/subscriptions/{ba.IdSubscription}/resourceGroups/{ba.NameResourceGroup}/providers/Microsoft.EventHub/namespaces/{nameNamespace}/authorizationrules/RootManageSharedAccessKey\"}},\"tags\":null}}";
-            string url = $"https://management.azure.com/subscriptions/{ba.IdSubscription}/providers/microsoft.insights/logprofiles/{nameLogProfile}?api-version=2016-03-01";
+            ActivityLogProfile logProfile = new ActivityLogProfile(ba.IdSubscription, ba.NameResourceGroup, nameNamespace);
+
+            string url = $"https://management.azure.com/subscriptions/{ba.IdSubscription}/providers/microsoft.insights/logprofiles/default?api-version=2016-03-01";
 
             AzureHttpClient ahc = new AzureHttpClient(ba.TokenAzure, ba.IdSubscription);
 
-            string logProfileErrorPut = await ahc.Test(HttpMethod.Put, url, body);
+            string logProfileErrorPut = await ahc.Test(HttpMethod.Put, url, JsonUtility.Serialize(logProfile));
 
             request.DataStore.AddToDataStore("nameEventHub", INSIGHTS_OPERATIONAL_LOGS, DataStoreType.Private);
 
---
+++ Source/Actions/Microsoft.Deployment.Common/Model/ActivityLog/ActivityLogProfileProperties.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+
+using Newtonsoft.Json;
+
+namespace Microsoft.Deployment.Common.Model.ActivityLog
+{
+    public class ActivityLogProfileProperties
+    {
+        [JsonProperty("categories")]
+        List<string> Categories = new List<string>() {
+            "Write",
+            "Delete",
+            "Action"
+        };
+        [JsonProperty("locations")]
+        List<string> Locations = new List<string>() {
+            "australiaeast",
+            "australiasoutheast",
+            "brazilsouth",
+            "canadacentral",
+            "canadaeast",
+            "centralindia",
+            "centralus",
+            "eastasia",
+            "eastus",
+            "eastus2",
+            "japaneast",
+            "japanwest",
+            "koreacentral",
+            "koreasouth",
+            "northcentralus",
+            "northeurope",
+            "southcentralus",
+            "southindia",
+            "southeastasia",
+            "uksouth",
+            "ukwest",
+            "westcentralus",
+            "westeurope",
+            "westindia",
+            "westus",
+            "westus2",
+            "global"
+        };
+        [JsonProperty("retentionPolicy")]
+        public ActivityLogProfileRetentionPolicy RetentionPolicy = new ActivityLogProfileRetentionPolicy();
+        [JsonProperty("serviceBusRuleId")]
+        public string ServiceBusRuleId;
+        [JsonProperty("storageAccountId")]
+        public string StorageAccountId;
+
+        public ActivityLogProfileProperties(string idSubscription, string nameResourceGroup, string nameNamespace)
+        {
+            ServiceBusRuleId = $"/subscriptions/{idSubscription}/resourceGroups/{nameResourceGroup}/providers/Microsoft.EventHub/namespaces/{nameNamespace}/authorizationrules/RootManageSharedAccessKey";
+        }
+    }
+}

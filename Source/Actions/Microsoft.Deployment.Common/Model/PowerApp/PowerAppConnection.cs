d802ce3eff2cc2ca72ffae473157f30d9c5b88bb
Yasin Shtiui <yashti@microsoft.com>
2017-11-07 15:41:45 -0800

    fix twitter powerapp

--- Source/Actions/Microsoft.Deployment.Common/Model/PowerApp/PowerAppConnection.cs
+++ Source/Actions/Microsoft.Deployment.Common/Model/PowerApp/PowerAppConnection.cs
@@ -10,10 +10,14 @@ namespace Microsoft.Deployment.Common.Model.PowerApp
           "[pbist_twitter].[twitter_query_details]",
           "[pbist_twitter].[twitter_query_readable]"
         };
+        [JsonProperty("dependencies")]
+        public string[] Dependencies = new string[] { };
         [JsonProperty("id")]
         public string Id = "/providers/microsoft.powerapps/apis/shared_sql";
         [JsonProperty("isOnPremiseConnection")]
         public bool IsOnPremiseConnection = false;
+        [JsonProperty("parameterHints")]
+        public object ParameterHints = null;
         [JsonProperty("sharedConnectionId")]
         public string SharedConnectionId;
 
---
+++ Source/Actions/Microsoft.Deployment.Common/Model/PowerApp/PowerAppConsentScopes.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+using Newtonsoft.Json;
+
+namespace Microsoft.Deployment.Common.Model.PowerApp
+{
+    public class PowerAppConsentScopes
+    {
+        [JsonProperty("will")]
+        public List<string> Will = new List<string>();
+
+        [JsonProperty("wont")]
+        public List<string> Wont = new List<string>();
+    }
+}

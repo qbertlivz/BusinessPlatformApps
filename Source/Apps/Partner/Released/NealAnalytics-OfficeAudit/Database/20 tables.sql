3ba46f34b959cd22dd26c8b57c49bb37c7b96ebb
Mohammad Ali (MOHAALI) <mohaali@microsoft.com>
2017-11-13 06:56:43 -0800

    made fixes to the audit template

--- Source/Apps/Partner/Released/NealAnalytics-OfficeAudit/Database/20 tables.sql
+++ Source/Apps/Partner/Released/NealAnalytics-OfficeAudit/Database/20 tables.sql
@@ -5,20 +5,6 @@ SET ANSI_NULL_DFLT_ON       ON;
 SET CONCAT_NULL_YIELDS_NULL ON;
 SET QUOTED_IDENTIFIER       ON;
 
-CREATE TABLE [adtlog].[audit_data](
-    [Id] [varchar](50) NOT NULL,
-    [RecordType] [varchar](max) NULL,
-    [CreationTime] [datetime] NOT NULL,
-    [UserType] [varchar](max) NULL,
-    [UserKey] [varchar](max) NULL,
-    [JSONPayload] [varchar](max) NULL,
-    [BatchId] [varchar](14) NULL,
-    PRIMARY KEY CLUSTERED 
-    (
-        [Id] ASC
-    )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)
-)
-
 CREATE TABLE [adtlog].[batch_log](
 	[BatchID] [varchar](14) NOT NULL,
 	[LogText] [nvarchar](max) NULL,
@@ -34,30 +20,6 @@ CREATE TABLE [adtlog].[batch_log](
 	[APIEndDate] [datetime] NULL
 )
 
-CREATE TABLE [adtlog].[datasets](
-    [DatasetId] [varchar](50) NOT NULL,
-    [DatasetName] [varchar](50) NOT NULL,
-    PRIMARY KEY CLUSTERED 
-    (
-        [DatasetId] ASC
-    )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)
-)
-
-CREATE TABLE [adtlog].[staging_audit_data](
-    [Id] [varchar](50) NOT NULL,
-    [RecordType] [varchar](max) NULL,
-    [CreationTime] [datetime] NOT NULL,
-    [UserType] [varchar](max) NULL,
-    [UserKey] [varchar](max) NULL,
-    [JSONPayload] [varchar](max) NULL,
-    [BatchId] [varchar](14) NULL
-)
-
-CREATE TABLE [adtlog].[staging_datasets](
-    [DatasetId] [varchar](50) NOT NULL,
-    [DatasetName] [varchar](50) NOT NULL
-)
-
 CREATE TABLE [adtlog].[user](
     [Email] [varchar](max) NULL,
     [Type] [varchar](10) NULL,
@@ -67,12 +29,3 @@ GO
 
 ALTER TABLE [adtlog].[user] ADD  DEFAULT (getdate()) FOR [EntryTime]
 GO
-
-CREATE NONCLUSTERED INDEX IDX_audit_data
-ON [adtlog].[audit_data] ([Id])
-
-CREATE NONCLUSTERED INDEX IDX_stagingAuditDataId
-ON [adtlog].[staging_audit_data] ([Id])
-
-CREATE NONCLUSTERED INDEX IDX_stagingDatasetsId
-ON [adtlog].[staging_datasets] ([DatasetId])

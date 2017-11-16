4c2986ba9f562e0421e74099db0fd3204b29f5ef
Dwayne Pryce <dwpryce@microsoft.com>
2017-11-16 13:10:08 -0800

    Intermediary checkin before I git clean -xfd; still have to write unit tests and verify the SocialGistApiKey retrieval works as well as the randomized schedule start time.

--- Source/Actions/Microsoft.Deployment.Actions.AzureCustom/Reddit/RetrieveSocialGistApiKey.cs
+++ Source/Actions/Microsoft.Deployment.Actions.AzureCustom/Reddit/RetrieveSocialGistApiKey.cs
@@ -8,13 +8,17 @@ using Microsoft.Deployment.Common;
 using Microsoft.Deployment.Common.ActionModel;
 using Microsoft.Deployment.Common.Actions;
 using Microsoft.Deployment.Common.ErrorCode;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Http.Headers;
+using Microsoft.Deployment.Common.Helpers;
 
 namespace Microsoft.Deployment.Actions.AzureCustom.Reddit
 {
     [Export(typeof(IAction))]
     public class RetrieveSocialGistApiKey : BaseAction
     {
-        private const string SocialGistKeyQueryParameterName = "key";
+        private const string SocialGistApiGenerationTemplate = "PowerBI";
 
         public override async Task<ActionResponse> ExecuteActionAsync(ActionRequest request)
         {
@@ -24,7 +28,8 @@ namespace Microsoft.Deployment.Actions.AzureCustom.Reddit
             {
                 var socialGistApiKey = await RetrieveKey(
                     Constants.SocialGistProvisionKeyUrl,
-                    Constants.SocialGistProvisionKeySecret
+                    Constants.SocialGistProvisionKeyUserName,
+                    Constants.SocialGistProvisionKeyPassphrase
                 );
                 // currently returns no value, which we then use and place into the AzureFunction AppSetting step.  Once you populate this with the real value from SocialGist, you shouldn't have to change
                 // the init.json
@@ -47,26 +52,97 @@ namespace Microsoft.Deployment.Actions.AzureCustom.Reddit
             }
         }
 
-        internal async Task<string> RetrieveKey(
+        public async Task<string> RetrieveKey(
             string socialGistKeyManagerUri,
-            string microsoftSocialGistKey
+            string socialGistProvisionKeyUserName,
+            string socialGistProvisionKeyPassphrase
         )
         {
-            return "";
-            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11;
-            using (var client = new System.Net.Http.HttpClient())
+            var description = Guid.NewGuid().ToString(); // this should be replaced later with something PII, but only after we have a proper UI and customer agreement to this effect
+            var queryParameters = new Dictionary<string, string>
             {
-                var socialGistUriBuilder = new UriBuilder(socialGistKeyManagerUri);
-                socialGistUriBuilder.Query = $"{HttpUtility.UrlEncode(SocialGistKeyQueryParameterName)}={HttpUtility.UrlEncode(microsoftSocialGistKey)}";
-                var response = await client.GetAsync(socialGistUriBuilder.Uri);
+                { "template", SocialGistApiGenerationTemplate },
+                { "rt", "json" },
+                { "description", description }
+            };
+            
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11;
+            using (var client = new HttpClient())
+            {
+
+                var httpRequestMessage = new HttpRequestMessage(HttpMethod.Post, socialGistKeyManagerUri);
+                httpRequestMessage.Headers.Authorization = BasicAuthenticationFromUsernameAndPassphrase(socialGistProvisionKeyUserName, socialGistProvisionKeyPassphrase);
+
+                var postPayload = new FormUrlEncodedContent(queryParameters);
+                httpRequestMessage.Content = postPayload;
+
+                var response = await client.SendAsync(httpRequestMessage);
                 if (!response.IsSuccessStatusCode)
                 {
                     throw new Exception("Unable to contact Social Gist key management service.");
                 }
-                var key = await response.Content.ReadAsStringAsync();
+                var jsonBody = await response.Content.ReadAsStringAsync();
                 // this is probably wrong, as it's almost definitely going to be a json response to us, not just a straight string
                 return key;
             }
         }
+
+        private AuthenticationHeaderValue BasicAuthenticationFromUsernameAndPassphrase(
+            string socialGistProvisionKeyUserName,
+            string socialGistProvisionKeyPassphrase
+        )
+        {
+            return new AuthenticationHeaderValue(
+                    "Basic",
+                    Convert.ToBase64String(
+                        Encoding.ASCII.GetBytes($"{socialGistProvisionKeyUserName}:{socialGistProvisionKeyPassphrase}")
+                    )
+                );
+        }
+
+        public string KeyFromJson(string jsonBody)
+        {
+            var obj = JsonUtility.GetJsonObjectFromJsonString(jsonBody);
+            var nodes = obj.SelectToken("$.response.Keys.Key[0]");
+            if (nodes == null)
+            {
+                return null;
+            }
+            return nodes.Value<string>("Key");
+        }
+    }
+
+    /*
+     * {
+    "response": {
+        "Request": {
+            "Actual": {
+                "rt": "json"
+            }
+        },
+        "Keys": {
+            "Key": [
+                {
+                    "Key": "6a7a33fa213b4fc701b24b76a57b01e2",
+                    "Description": "Microsoft PowerBI - DwayneTest1",
+                    "Enabled": 1,
+                    "RequestsPerDay": "1000",
+                    "DataCounters": {
+                        "MaxMatches": "10000",
+                        "MaxDataSet": "100",
+                        "MaxResultsDays": "No Limit"
+                    },
+                    "DataSources": {
+                        "SelectedSources": "",
+                        "DedicatedContentProviderId": 245
+                    }
+                }
+            ]
+        },
+        "Timer": []
     }
 }
+*/
+}
+ 
+ 
---
+++ Source/Test/Microsoft.Deployment.Tests.Actions/Reddit/RetrieveSocialGistApiKeyTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Deployment.Actions.AzureCustom.Reddit;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Deployment.Tests.Actions.Reddit
+{
+    [TestClass]
+    class RetrieveSocialGistApiKeyTests
+    {
+
+        [TestMethod]
+        public void TestKeyFromJson_works()
+        {
+            var sample = "{\"response\":{\"Request\":{\"Actual\":{\"rt\":\"json\"}},\"Keys\":{\"Key\":[{\"Key\":\"THISISNOTAKEYTHISISATRIBUTE\",\"Description\":\"Microsoft PowerBI - DwayneTest1\",\"Enabled\":1,\"RequestsPerDay\":\"1000\",\"DataCounters\":{\"MaxMatches\":\"10000\",\"MaxDataSet\":\"100\",\"MaxResultsDays\":\"No Limit\"},\"DataSources\":{\"SelectedSources\":\"\",\"DedicatedContentProviderId\":245}}]},\"Timer\":[]}}";
+            var retriever = new RetrieveSocialGistApiKey();
+            var key = retriever.KeyFromJson(sample);
+            Assert.AreEqual("THISISNOTAKEYTHISISATRIBUTE", key);
+        }
+
+        [TestMethod]
+        public void TestKeyFromJson_malformed_json()
+        {
+
+        }
+
+    }
+}

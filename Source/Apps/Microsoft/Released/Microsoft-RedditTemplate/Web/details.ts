0284dc215cf8691f1aba36704a6cf4756e7d9c78
Dwayne Pryce <dwpryce@microsoft.com>
2017-11-28 15:46:19 -0800

    Added another page full of some text (and a placeholder) for SocialGist.  Added [Preview] tags to prominent titles.  Emphasized SocialGist API Key deactivation date in multiple places.  Updated SQL DB to change controversiality from boolean to a float.  Added 3 PBIXes, 2 of which are current placeholders, for a zipped/packaged download of the 3 workbook usages.  Updated minor release of FunctionApp to 0.7.x.

---
+++ Source/Apps/Microsoft/Released/Microsoft-RedditTemplate/Web/details.ts
@@ -0,0 +1,7 @@
+﻿import { ViewModelBase } from '../../../../../SiteCommon/Web/services/view-model-base'
+
+export class Details extends ViewModelBase {
+    async onLoaded(): Promise<void> {
+        this.isValidated = true;
+    }
+}

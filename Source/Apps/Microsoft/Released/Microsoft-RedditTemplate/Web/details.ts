ca6334ca72994c675c8c9760800f1c73569aacc8
Jon McLean <jon.mclean@atsid.com>
2017-12-12 11:19:46 -0800

    Started work on validation

--- Source/Apps/Microsoft/Released/Microsoft-RedditTemplate/Web/details.ts
+++ Source/Apps/Microsoft/Released/Microsoft-RedditTemplate/Web/details.ts
@@ -2,6 +2,14 @@
 
 export class Details extends ViewModelBase {
     async onLoaded(): Promise<void> {
-        this.isValidated = true;
+        // this.isValidated = false;
+    }
+
+    async onValidate(): Promise<boolean> {
+        super.onInvalidate();
+
+        this.MS.ErrorService.set(`Must have between 0 and 32 entity aliases in total.`);
+
+        return this.isValidated;
     }
 }

2abe2661f332547aa64753d9f8822625d8679d89
Jon McLean <jon.mclean@atsid.com>
2017-12-12 12:34:02 -0800

    Added basic validation

--- Source/Apps/Microsoft/Released/Microsoft-RedditTemplate/Web/details.ts
+++ Source/Apps/Microsoft/Released/Microsoft-RedditTemplate/Web/details.ts
@@ -1,6 +1,9 @@
 ﻿import { ViewModelBase } from '../../../../../SiteCommon/Web/services/view-model-base'
+import { Registration } from './registration';
 
 export class Details extends ViewModelBase {
+    registration: Registration = new Registration();
+
     async onLoaded(): Promise<void> {
         // this.isValidated = false;
     }
@@ -8,7 +11,33 @@ export class Details extends ViewModelBase {
     async onValidate(): Promise<boolean> {
         super.onInvalidate();
 
-        this.MS.ErrorService.set(`Must have between 0 and 32 entity aliases in total.`);
+        let error: string = "";
+
+        if (this.registration.nameFirst.length === 0 ||
+            this.registration.nameLast.length === 0 ||
+            this.registration.company.length === 0 ||
+            this.registration.jobTitle.length === 0 ||
+            this.registration.descriptionOfUse.length === 0) {
+            error += "All fields are required.";
+        }
+
+        if (this.registration.emailConfirmation.length === 0 ||
+            this.registration.email.length === 0 ||
+            this.registration.email !== this.registration.emailConfirmation ||
+            this.registration.email.indexOf('@') === -1) {
+            error += " Email and Email Confirmation are both required and must match";
+        }
+
+        if (!this.registration.accepted) {
+            error += " You must accept the correspondence terms";
+        }
+
+        if (error.length !== 0) {
+            this.MS.ErrorService.message = error;
+            this.isValidated = false;
+        } else {
+            this.isValidated = true;
+        }
 
         return this.isValidated;
     }

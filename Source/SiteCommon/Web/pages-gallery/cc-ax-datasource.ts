3f0e690de67e53570ca670ec6a4afd14d72981ea
skg-marimuthu <34898649+skg-marimuthu@users.noreply.github.com>
2018-06-22 16:21:07 -0700

    Added CDSA Storage, Data source details

---
+++ Source/SiteCommon/Web/pages-gallery/cc-ax-datasource.ts
@@ -0,0 +1,32 @@
+﻿import { ViewModelBase } from '../services/view-model-base';
+import { RouteConfig } from 'aurelia-router';
+
+export class AxDatasource extends ViewModelBase {
+    router: RouteConfig[] = [];
+
+    async onLoaded(): Promise<void> {
+        super.onLoaded();
+        
+        this.router = [{
+            href: window.location.href.replace(window.location.hash, '#/ConnecttoDynamics365'),
+            name: 'Dynamics365OnCloud',
+            nav: true,
+            route: 'Dynamics365OnCloud',
+            //title: 'Dynamics 365 Finance & Operations On Cloud'
+            title: 'Dynamics 365 F&O On Cloud'
+        },
+        {
+            name: 'Dynamics365OnPremise',
+            nav: false,
+            route: 'Dynamics365OnPremise',
+            //title: 'Dynamics 365 Finance & Operations On Premise'
+            title: 'Dynamics 365 F&O On Premise'
+        },
+        {
+            name: 'DynamicsAX2012',
+            nav: false,
+            route: 'DynamicsAX2012',
+            title: 'Dynamics AX2012 R3'
+        }];
+    }
+}
